d9738cc2486bb0ab9361135970dd06a92d838f44
Alexandre Juca <corextechnologies@gmail.com>
2017-12-29 18:54:47 +0100

    Added basic validation to Artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,7 @@
 class Artist < ApplicationRecord
     has_many :tracks
     has_many :albums
+
+    validates :name, length => { :minumum => 2},: presence => true
+    validates :description, length => { :minumum => 2},: presence => true
 end
